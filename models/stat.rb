2b06c39e75e92e26a3ba5c37488eada1688cb80b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-05-05 21:55:28 +0900

    Add property

--- models/stat.rb
+++ models/stat.rb
@@ -4,7 +4,8 @@ class Stat
   # property <name>, <type>
   property :id, Serial
   property :chart_id, Integer, :unique_index => :chart_key_time, :required => true
-  property :value, Float, :unique_index => :chart_key_time, :required => true
+  property :key, String, :unique_index => :chart_key_time, :required => true
+  property :value, Float, :required => true
 
   timestamps :at
   property :deleted_at, ParanoidDateTime, :unique_index => :chart_key_time
