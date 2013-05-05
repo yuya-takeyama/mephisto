a9c14efde6e4270d029354c8eeb15775513648ac
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-05-05 21:56:57 +0900

    Fix property name

--- models/stat.rb
+++ models/stat.rb
@@ -3,10 +3,10 @@ class Stat
 
   # property <name>, <type>
   property :id, Serial
-  property :chart_id, Integer, :unique_index => :chart_key_time, :required => true
-  property :key, String, :unique_index => :chart_key_time, :required => true
+  property :chart_id, Integer, :unique_index => :chart_label_time, :required => true
+  property :label, String, :unique_index => :chart_label_time, :required => true
   property :value, Float, :required => true
 
   timestamps :at
-  property :deleted_at, ParanoidDateTime, :unique_index => :chart_key_time
+  property :deleted_at, ParanoidDateTime, :unique_index => :chart_label_time
 end
